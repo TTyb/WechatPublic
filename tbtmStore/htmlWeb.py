8cc326b3652ea963ef97737aa4e8d67e9aee552c
TTyb <420439007@qq.com>
2019-04-17 19:45:57 +0800

    store

---
+++ tbtmStore/htmlWeb.py
@@ -0,0 +1,128 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from flask import Flask, request, render_template
+import json
+import requests
+
+
+def getHtmlJson(id):
+    result_arr = []
+    headers = {
+        'accept': '*/*',
+        'accept-encoding': 'gzip, deflate, br',
+        'accept-language': 'zh-CN,zh;q=0.9',
+        'referer': 'https://uland.taobao.com/semm/tbsearch?refpid=mm_26632258_3504122_32554087&keyword=%E5%A5%B3%E8'
+                   '%A3%85 '
+                   '&rewriteQuery=1&a=mi={imei}&sms=baidu&idfa={'
+                   'idfa}&clk1=abab6283306413775910d4b0b37ca047&upsid=abab6283306413775910d4b0b37ca047',
+        'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) '
+                      'Chrome/%s Mobile Safari/537.36',
+    }
+    session = requests.Session()
+    url = r'https://h5api.m.taobao.com/h5/mtop.taobao.detail.getdetail/6.0/?jsv=2.4.8&appKey=12574478&t=1535083295045' \
+          r'&sign=ef22a6dc765bd6ce86d36e2ba9a6cc33&api=mtop.taobao.detail.getdetail&v=6.0&dataType=jsonp&ttid=2017' \
+          r'%40taobao_h5_6.6.0&AntiCreep=true&type=jsonp&callback=mtopjsonp2&data=%7B%22itemNumId%22%3A%22' + str(
+        id) + r'%22%7D '
+    # print(url)
+    r = session.get(url=url, headers=headers)
+    html = r.text
+    start = html.find('(')
+    datas = (json.loads(html[start + 1:-1]))['data']
+
+    #----------------------------------
+    # 商品名
+    title = datas['item']['title']
+    skus=datas['skuBase']['skus']
+    props=datas['skuBase']['props']
+
+
+    # 商品现有价格和库存
+    skuCore = json.loads(datas['apiStack'][0]['value'])['skuCore']['sku2info']
+    for skuId in skuCore.keys():
+        if skuId != '0':
+            result_dict = {}
+            price = skuCore[skuId]['price']['priceText']
+            quantity = skuCore[skuId]['quantity']
+            result_dict["id"] = id
+            result_dict["skuId"] = skuId
+            result_dict["名字"] = title
+            result_dict["价格"] = price
+            result_dict["库存"] = quantity
+            for item in skus:
+                if skuId==item["skuId"]:
+                    propPath = item["propPath"].split(";")
+                    size_id = propPath[0].split(":")
+                    colour_id = propPath[1].split(":")
+                    for itm in props:
+                        if size_id[0]==itm["pid"]:
+                            for it in itm["values"]:
+                                if size_id[1]==it["vid"]:
+                                    result_dict[itm["name"]] = it["name"]
+                        elif colour_id[0]==itm["pid"]:
+                            for it in itm["values"]:
+                                if colour_id[1] == it["vid"]:
+                                    result_dict[itm["name"]] = it["name"]
+                                    result_dict["image"] = "https:" + it["image"]
+            result_arr.append(result_dict)
+
+    return result_arr
+
+
+
+from io import BytesIO
+import xlsxwriter
+import time
+def create_workbook():
+    output = BytesIO()
+    workbook = xlsxwriter.Workbook(output, {'in_memory': True})
+    # 设置Sheet的名字为download
+    worksheet = workbook.add_worksheet('download')
+    # 列首
+    title = ["图片","id","skuId","名字","尺码","颜色","价格","库存"]
+    worksheet.write_row('A1', title)
+    dictList = getHtmlJson(id)
+    for i in range(len(dictList)):
+        row = [dictList[i]["id"],dictList[i]["skuId"],dictList[i]["名字"],dictList[i]["尺码"],dictList[i]["颜色"],dictList[i]["价格"],dictList[i]["库存"]]
+        worksheet.write_row('B' + str(i + 2), row)
+        # 缓存图片
+        image_data = BytesIO(request.urlopen(dictList[i]["image"]).read())
+        # 插入图片
+        worksheet.insert_image('B' + str(i + 1), dictList[i]["image"], {'image_data': image_data})
+        i += 1
+        time.sleep(2)
+    workbook.close()
+    response = make_response(output.getvalue())
+    output.close()
+    return response
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    dictList = []
+    if request.method == 'GET':
+        return render_template("index.html", dictList=dictList)
+
+    elif request.method == 'POST':
+        input_id = request.form["goods_id"]
+        for i_id in input_id.split("\n"):
+            # dictList.append(getHtmlJson(i_id))
+            dictList += getHtmlJson(i_id)
+        return render_template("index.html", dictList=dictList)
+
+
+from flask import make_response
+@app.route('/download', methods=['GET'])
+def download():
+
+    response = create_workbook()
+    response.headers['Content-Type'] = "utf-8"
+    response.headers["Cache-Control"] = "no-cache"
+    response.headers["Content-Disposition"] = "attachment; filename=download.xlsx"
+    return response
+
+if __name__ == "__main__":
+    app.run(host='127.0.0.1', port=99, debug=True)
+    # app.run(debug=True)
