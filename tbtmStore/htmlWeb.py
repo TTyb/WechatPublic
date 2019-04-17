83d86fd1a4e18b25ce2cf384b4822208a42df48a
TTyb <420439007@qq.com>
2019-04-17 21:50:15 +0800

    TTyb

--- tbtmStore/htmlWeb.py
+++ tbtmStore/htmlWeb.py
@@ -124,5 +124,5 @@ def download():
     return response
 
 if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=99, debug=True)
+    app.run(host='127.0.0.1', port=5000, debug=True)
     # app.run(debug=True)
