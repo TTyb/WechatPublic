d714c5d51f810d6f98e1d86eeedc49445e879c4d
TTyb <420439007@qq.com>
2019-05-16 11:58:26 +0800

    tbtmseller

---
+++ tbtmSeller/GetCookie.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+import requests
+
+
+def get_Cookie2_t_token(shop_id,session):
+    headers = {
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+        'accept-encoding': 'gzip, deflate',
+        'accept-language': 'zh-cn,zh;q=0.8,en-us;q=0.5,en;q=0.3',
+        'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) '
+                      'Chrome/%s Mobile Safari/537.36',
+        'host': 'login.taobao.com'
+    }
+    url = "https://login.taobao.com/jump?target=https://ruiducp.m.tmall.com/?tbpm=1&ajson=1&parentCatId=0&shop_id=" + shop_id
+    session.get(url=url, headers=headers)
+    html_set_cookie = requests.utils.dict_from_cookiejar(session.cookies)
+    return html_set_cookie
+
+def get_m_h5_tk(shop_id,session):
+    headers = {
+        'accept': '*/*',
+        'accept-encoding': 'gzip, deflate, br',
+        'accept-language': 'zh-CN,zh;q=0.9',
+        'referer': 'https://ruiducp.m.tmall.com/?ajson=1&parentCatId=0&shop_id=' + shop_id,
+        'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) '
+                      'Chrome/%s Mobile Safari/537.36',
+        'host': 'h5api.m.tmall.com'
+    }
+    _m_h5_tk_url = 'https://h5api.m.tmall.com/h5/mtop.shop.render.getpageview/1.0/?jsv=2.4.8&appKey=12574478&t=1557914653186&sign=660afbc5af93a17f28721a1be5d7110c&api=mtop.shop.render.getpageview&v=1.0&type=jsonp&dataType=jsonp&callback=mtopjsonp1&data={}'
+    cookie = {'isg': 'BC4ucq8U_sXxaQohN_Z3oCUNfITwL_IpGTZBSFj3mjHsO86VwL9COdQ992Fy-OpB',
+              't': get_Cookie2_t_token(shop_id,session)["t"], '_tb_token_': get_Cookie2_t_token(shop_id,session)["_tb_token_"],
+              "cookie2": get_Cookie2_t_token(shop_id,session)["cookie2"]}
+    session.get(_m_h5_tk_url, headers=headers, cookies=cookie)
+    html_set_cookie = requests.utils.dict_from_cookiejar(session.cookies)
+    enc_url = "https://h5api.m.tmall.com/h5/mtop.taobao.geb.enhenced.itemlist.get/2.0/?jsv=2.4.8&appKey=12574478&t=1557972233869&sign=02d52c49694c3d90d08ff9f4afaa8a67&api=mtop.taobao.geb.enhenced.itemlist.get&v=2.0&type=originaljson&timeout=3000&AntiCreep=true&dataType=json&H5Request=true&data={\"ownerId\":\"\",\"itemIds\":\"\",\"pageSize\":1,\"isAuto\":false}"
+    session.get(enc_url, headers=headers, cookies=cookie)
+    enc_set_cookie = requests.utils.dict_from_cookiejar(session.cookies)
+    html_set_cookie["enc"]=enc_set_cookie["enc"]
+    return html_set_cookie
+
+
+# 获取所有的cookie
+def getAllCookies(shop_id,session):
+    Cookie2_t_token = get_Cookie2_t_token(shop_id,session)
+    m_h5_tk = get_m_h5_tk(shop_id,session)
+
+    cookie = {'isg': 'BEJCODheqsbMmraaY3R3Z_hKkEikE0Yt5dLdFIxbbrVg3-JZdKOWPchZix0GlL7F',
+              'enc': m_h5_tk["enc"],
+              't': Cookie2_t_token["t"], '_tb_token_': Cookie2_t_token["_tb_token_"],
+              "cookie2": Cookie2_t_token["cookie2"], '_m_h5_tk_enc': m_h5_tk["_m_h5_tk_enc"],
+              '_m_h5_tk': m_h5_tk["_m_h5_tk"]}
+    return cookie
---
+++ tbtmSeller/getAllGoods.py
@@ -0,0 +1,64 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import requests
+import random
+import json
+import time
+from GetCookie import *
+
+session = requests.session()
+
+
+def getJson(shop_id, page):
+    headers = {
+        'accept': '*/*',
+        'accept-encoding': 'gzip, deflate, br',
+        'accept-language': 'zh-CN,zh;q=0.9',
+        'referer': 'https://ruiducp.m.tmall.com/shop/shop_auction_search.htm?&shop_id=' + shop_id + '&sort=default',
+        'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) '
+                      'Chrome/%s Mobile Safari/537.36',
+        'host': 'ruiducp.m.tmall.com'
+    }
+    num = str(random.randint(83739921, 87739530))
+    url = "https://ruiducp.m.tmall.com/shop/shop_auction_search.do?spm=a2141.7631565.0.0.496714bbs5vOWO&sort=s&p="+str(page)+"&page_size=12&from=h5&&shop_id="+shop_id+"ajson=1&_tm_source=tmallsearch&callback=jsonp_" + num
+    cookie = getAllCookies(shop_id,session)
+    r = session.get(url, headers=headers, cookies=cookie)
+    html = r.text
+    return html
+
+
+def writeJson(html,page):
+    resultJson = {}
+    start = html.find('(')
+    resultJson[str(page)] = json.loads(html[start + 1:-1])
+    print(resultJson)
+
+    file = open("./json", "a", encoding="utf-8")
+    file.write(str(resultJson) + "\n")
+    file.close()
+
+
+def getAllPage(shop_id):
+    # 获取总页数
+    html=getJson(shop_id, 1)
+    start = html.find('(')
+    total_page = int((json.loads(html[start + 1:-1]))['total_page'])
+    # 写入本地
+    writeJson(html, 1)
+
+    # 实现循环抓取
+    for i in range(2, total_page + 1):
+        range_html = getJson(shop_id,i)
+        # 写入本地
+        writeJson(range_html, i)
+        time.sleep(random.randint(2, 4))
+
+def main():
+    shop_id = "381682363"
+    getAllPage(shop_id)
+
+
+if __name__ == '__main__':
+    main()
+    # https://ruiducp.m.tmall.com/?shop_id=381682363
