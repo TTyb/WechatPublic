258ae1f1cdae6c2f3bd8adc2b73f8a1dd7506bdc
TTyb <420439007@qq.com>
2019-06-06 10:06:56 +0800

    seller

--- tbtmSeller/GetCookie.py
+++ tbtmSeller/GetCookie.py
@@ -19,6 +19,22 @@ def get_Cookie2_t_token(shop_name,shop_id,session):
     html_set_cookie = requests.utils.dict_from_cookiejar(session.cookies)
     return html_set_cookie
 
+def get_cna(shop_name,shop_id,session):
+    headers = {
+        'accept': '*/*',
+        'accept-encoding': 'gzip, deflate',
+        'accept-language': 'zh-cn,zh;q=0.8,en-us;q=0.5,en;q=0.3',
+        'referer': 'https://' + str(shop_name) + '.m.tmall.com/?ajson=1&parentCatId=0&shop_id=' + shop_id,
+        'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) '
+                      'Chrome/%s Mobile Safari/537.36',
+        'host': 'log.mmstat.com'
+    }
+    cna_url="https://log.mmstat.com/eg.js"
+    session.get(cna_url, headers=headers)
+    cna_set_cookie = requests.utils.dict_from_cookiejar(session.cookies)
+    return cna_set_cookie
+
+
 def get_m_h5_tk(shop_name,shop_id,session):
     headers = {
         'accept': '*/*',
@@ -30,26 +46,30 @@ def get_m_h5_tk(shop_name,shop_id,session):
         'host': 'h5api.m.tmall.com'
     }
     _m_h5_tk_url = 'https://h5api.m.tmall.com/h5/mtop.shop.render.getpageview/1.0/?jsv=2.4.8&appKey=12574478&t=1557914653186&sign=660afbc5af93a17f28721a1be5d7110c&api=mtop.shop.render.getpageview&v=1.0&type=jsonp&dataType=jsonp&callback=mtopjsonp1&data={}'
-    cookie = {'isg': 'BC4ucq8U_sXxaQohN_Z3oCUNfITwL_IpGTZBSFj3mjHsO86VwL9COdQ992Fy-OpB',
+    cookie = {'isg': 'BM7OlBasHs5cfKrqq4Bn5IWqHKSQT5JJudahaPgXOlGMW261YN_iWXQZl0GS2Iph',
               't': get_Cookie2_t_token(shop_name,shop_id,session)["t"], '_tb_token_': get_Cookie2_t_token(shop_name,shop_id,session)["_tb_token_"],
               "cookie2": get_Cookie2_t_token(shop_name,shop_id,session)["cookie2"]}
     session.get(_m_h5_tk_url, headers=headers, cookies=cookie)
     html_set_cookie = requests.utils.dict_from_cookiejar(session.cookies)
+
+    # 以下是获取enc
+    cna_set_cookie=get_cna(shop_name, shop_id, session)
     enc_url = "https://h5api.m.tmall.com/h5/mtop.taobao.geb.enhenced.itemlist.get/2.0/?jsv=2.4.8&appKey=12574478&t=1557972233869&sign=02d52c49694c3d90d08ff9f4afaa8a67&api=mtop.taobao.geb.enhenced.itemlist.get&v=2.0&type=originaljson&timeout=3000&AntiCreep=true&dataType=json&H5Request=true&data={\"ownerId\":\"\",\"itemIds\":\"\",\"pageSize\":1,\"isAuto\":false}"
+    cookie["cna"]=cna_set_cookie["cna"]
     session.get(enc_url, headers=headers, cookies=cookie)
     enc_set_cookie = requests.utils.dict_from_cookiejar(session.cookies)
     html_set_cookie["enc"]=enc_set_cookie["enc"]
     return html_set_cookie
 
-
 # 获取所有的cookie
 def getAllCookies(shop_name,shop_id,session):
     Cookie2_t_token = get_Cookie2_t_token(shop_name,shop_id,session)
     m_h5_tk = get_m_h5_tk(shop_name,shop_id,session)
+    cna_set_cookie = get_cna(shop_name, shop_id, session)
 
-    cookie = {'isg': 'BLCw7c-nmEVcK0RBYGON1IbJgn4C-ZRDqxgPAqoBfIveZVAPUglk0wYTuaugbkwb',
+    cookie = {'isg': 'BM7OlBasHs5cfKrqq4Bn5IWqHKSQT5JJudahaPgXOlGMW261YN_iWXQZl0GS2Iph',
               'enc': m_h5_tk["enc"],
               't': Cookie2_t_token["t"], '_tb_token_': Cookie2_t_token["_tb_token_"],
               "cookie2": Cookie2_t_token["cookie2"], '_m_h5_tk_enc': m_h5_tk["_m_h5_tk_enc"],
-              '_m_h5_tk': m_h5_tk["_m_h5_tk"]}
+              '_m_h5_tk': m_h5_tk["_m_h5_tk"],"cna":cna_set_cookie["cna"]}
     return cookie
--- tbtmSeller/getAllGoods.py
+++ tbtmSeller/getAllGoods.py
@@ -8,31 +8,34 @@ import time
 from GetCookie import *
 import math
 
-session = requests.session()
 
-
-def getJson(shop_name,shop_id, page):
-    headers = {
-        'accept': '*/*',
-        'accept-encoding': 'gzip, deflate, br',
-        'accept-language': 'zh-CN,zh;q=0.9',
-        #'referer': 'https://'+str(shop_name)+'.m.tmall.com/shop/shop_auction_search.htm?&sort=default',
-        'referer': 'https://'+str(shop_name)+'.m.tmall.com/shop/shop_auction_search.htm?&shop_id=' + shop_id + '&sort=default',
-        'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) '
-                      'Chrome/%s Mobile Safari/537.36',
-        'host': str(shop_name)+'.m.tmall.com'
-    }
-    # 'jsonp_' + Date.now() % 100000 + Math.ceil(Math.random() * 1000)
-    num = str(int(time.time() * 1000)%100000+math.ceil(random.random()*1000))
-    url = "https://"+str(shop_name)+".m.tmall.com/shop/shop_auction_search.do?spm=a2141.7631565.0.0.6f2114bbPEH5Za&sort=s&p="+str(page)+"&page_size=12&from=h5&ajson=1&_tm_source=tmallsearch&callback=jsonp_" + num
-    #url = "https://"+str(shop_name)+".m.tmall.com/shop/shop_auction_search.do?spm=a2141.7631565.0.0.496714bbs5vOWO&sort=s&p="+str(page)+"&page_size=12&from=h5&shop_id="+shop_id+"&ajson=1&_tm_source=tmallsearch&callback=jsonp_" + num
+def getJson(shop_name, shop_id, page):
+    num = str(int(time.time() * 1000) % 100000) + str(math.ceil(random.random() * 1000))
+    url = "https://"+str(shop_name)+".m.tmall.com/shop/shop_auction_search.do?sort=s&p="+str(page)+"&page_size=12&from=h5&shop_id="+str(shop_id)+"&ajson=1&_tm_source=tmallsearch&callback=jsonp_" + num
+    # url = "https://ruiducp.m.tmall.com/shop/shop_auction_search.do?sort=s&p=1&page_size=12&from=h5&shop_id=381682363&ajson=1&_tm_source=tmallsearch&callback=jsonp_" + num
+    print(url)
+    session = requests.session()
+    # 这里可以不用生成的cookie，可以用自己得到的cookie
     cookie = getAllCookies(shop_name,shop_id,session)
-    r = session.get(url, headers=headers, cookies=cookie)
+
+    headers = {
+        'Accept': '*/*',
+        'Accept-Encoding': 'gzip, deflate',
+        'Accept-Language': 'zh-cn,zh;q=0.8,en-us;q=0.5,en;q=0.3',
+        'Host': str(shop_name) + '.m.tmall.com',
+        'Connection': 'keep-alive',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:32.0) Gecko/20100101 Firefox/32.0'
+    }
+
+    r = session.get(url, headers=headers,cookies=cookie)
+    print(r.request.headers)
+    print(r.history)
+    print(r.status_code)
     html = r.text
     return html
 
 
-def writeJson(html,page):
+def writeJson(html, page):
     resultJson = {}
     start = html.find('(')
     resultJson[str(page)] = json.loads(html[start + 1:-1])
@@ -42,9 +45,10 @@ def writeJson(html,page):
     file.close()
 
 
-def getAllPage(shop_name,shop_id):
+def getAllPage(shop_name, shop_id):
     # 获取总页数
-    html=getJson(shop_name,shop_id, 1)
+    html = getJson(shop_name, shop_id, 1)
+    print(html)
     start = html.find('(')
     total_page = int((json.loads(html[start + 1:-1]))['total_page'])
     # 写入本地
@@ -52,16 +56,17 @@ def getAllPage(shop_name,shop_id):
 
     # 实现循环抓取
     for i in range(2, total_page + 1):
-        range_html = getJson(shop_name,shop_id,i)
+        range_html = getJson(shop_name, shop_id, i)
         # 写入本地
         writeJson(range_html, i)
         time.sleep(random.randint(2, 4))
 
+
 def main():
     # 输入店铺名和店铺的id
-    shop_name="ruiducp"
+    shop_name = "ruiducp"
     shop_id = "381682363"
-    getAllPage(shop_name,shop_id)
+    getAllPage(shop_name, shop_id)
 
 
 if __name__ == '__main__':
